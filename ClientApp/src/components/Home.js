20737702c2787609e575e253d3ae3bf61eddc427
JPageCoreBTS <125911105+JPageCoreBTS@users.noreply.github.com>
2023-02-27 17:21:47 -0600

    we love azure

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -6,8 +6,10 @@ export class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
+        <div class="jumbotron">
+          <h1>.NET 💜 Azure</h1>
+          <p class="lead">Example .NET app to Azure App Service.</p>
+        </div>
         <ul>
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
           <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
